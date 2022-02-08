b771e860d2a9aa67e89a86294f98d4a3d75892e2
Gibert Quentin <quentingibert6@gmail.com>
2022-02-08 15:56:10 +0100

    correction des avertissements

--- src/App.js
+++ src/App.js
@@ -1,87 +1,96 @@
 import Notes from "./Notes";
 import TodoList from "./Todo-list";
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from "react";
 
 const getLocalStorage = () => {
-  const storage = localStorage.getItem('tache')
-  if(storage) {
-    return JSON.parse(storage)
-  }else {
-    return []
+  const storage = localStorage.getItem("tache");
+  if (storage) {
+    return JSON.parse(storage);
+  } else {
+    return [];
   }
-
-}
-
+};
 
 function App() {
-  const  [userInput, setUserInput] = useState('')
-    const [desc, setDesc] = useState('')
-    const  [item, setItem] = useState(getLocalStorage())
-    const [toggleSubmit, setToggleSubmit] = useState(true)
-    const [isEditItem, setIsEditItem] = useState(null)
+  const [userInput, setUserInput] = useState("");
+  const [desc, setDesc] = useState("");
+  const [item, setItem] = useState(getLocalStorage());
+  const [toggleSubmit, setToggleSubmit] = useState(true);
+  const [isEditItem, setIsEditItem] = useState(null);
 
-useEffect(()=> {
-  localStorage.setItem('tache', JSON.stringify(item))
-}, [item])
+  useEffect(() => {
+    localStorage.setItem("tache", JSON.stringify(item));
+  }, [item]);
 
-    const modifTodo = id => {
-      const newTodo = item.find((elem) => {
-        return elem.id === id 
-        
-    })
-    setToggleSubmit(false)
-    setUserInput(newTodo.userInput)
-    setIsEditItem(id)
+  const modifTodo = (id) => {
+    const newTodo = item.find((elem) => {
+      return elem.id === id;
+    });
+    setToggleSubmit(false);
+    setUserInput(newTodo.userInput);
+    setIsEditItem(id);
+  };
 
-    }
-    
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        if(!userInput) {
-          alert ('veuillez ajouter une tâche')
-        }else if (userInput && !toggleSubmit) {
-          setItem(
-            item.map((elem) => {
-              if(elem.id === isEditItem) {
-                return {...item, id : elem.id, desc: desc , userInput:userInput}
-              }
-              return elem
-            })
-          )
-    setToggleSubmit(true)
-    setUserInput('')
-    setIsEditItem(null)
-        }else {
-          if (userInput || desc) {
-            const newInput = { id: new Date().getTime().toString(),userInput, desc };
-      
-            setItem([...item, newInput]);
-        }
-        }
-        
-        console.log(item);
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!userInput) {
+      alert("veuillez ajouter une tâche");
+    } else if (userInput && !toggleSubmit) {
+      setItem(
+        item.map((elem) => {
+          if (elem.id === isEditItem) {
+            return { ...item, id: elem.id, desc: desc, userInput: userInput };
+          }
+          return elem;
+        })
+      );
+      setToggleSubmit(true);
+      setUserInput("");
+      setDesc("");
+      setIsEditItem(null);
+    } else {
+      if (userInput || desc) {
+        const newInput = {
+          id: new Date().getTime().toString(),
+          userInput,
+          desc,
+        };
 
-    const removeTodo = id => {
-      const removeArr = [...item].filter(userInput => userInput.id !==id)
-      setItem(removeArr)
-    }
-
-    
-      const del = () => {
-        setItem([])
+        setItem([...item, newInput]);
       }
-    
+    }
+
+    console.log(item);
+  };
+
+  const removeTodo = (id) => {
+    const removeArr = [...item].filter((userInput) => userInput.id !== id);
+    setItem(removeArr);
+  };
+
+  const del = () => {
+    setItem([]);
+  };
+
   return (
     <>
-    <div>
-      <h1>Todo List</h1>
-    </div>
-    <TodoList handleSubmit ={handleSubmit} desc={desc} userInput={userInput} setDesc={setDesc} setUserInput={setUserInput} toggleSubmit={toggleSubmit} />
-    <Notes item = {item} removeTodo ={removeTodo} modifTodo={modifTodo} />
-    <button className="btn-result" onClick={() => del([]) }>Tout delete</button>
+      <div>
+        <h1>Todo List React</h1>
+      </div>
+      <TodoList
+        handleSubmit={handleSubmit}
+        desc={desc}
+        userInput={userInput}
+        setDesc={setDesc}
+        setUserInput={setUserInput}
+        toggleSubmit={toggleSubmit}
+      />
+      <Notes item={item} removeTodo={removeTodo} modifTodo={modifTodo} />
+      <button className="btn-result" onClick={() => del([])}>
+        Tout delete
+      </button>
     </>
-    )
+  );
 }
 
 export default App;
--- src/Notes.js
+++ src/Notes.js
@@ -1,27 +1,26 @@
-import React from 'react';
-import TodoList from './Todo-list';
-import App from './App';
+import React from "react";
 
-const Notes = ({item, removeTodo, modifTodo}) => {
-
+const Notes = ({ item, removeTodo, modifTodo }) => {
   return (
-      <div>{item.map((note) => {
-        const {id, desc, userInput,} = note
-        
+    <div>
+      {item.map((note) => {
+        const { id, desc, userInput } = note;
+
         return (
-        
-        
-            <div className='result' key={id}>
-                <h4>{userInput}</h4>
-                <p>{desc}</p>
-                <button onClick={() => removeTodo(id)} className='btn'><i className="far fa-trash-alt"></i></button>
-            <button onClick={() => modifTodo(id)} className='btn'><i className="fas fa-pencil-alt"></i></button>
-            </div>
-            
-        )
-  })}</div>
-      
-  )
+          <div className="result" key={id}>
+            <h4>{userInput}</h4>
+            <p>{desc}</p>
+            <button onClick={() => modifTodo(id)} className="btn">
+              <i className="fas fa-pencil-alt"></i>
+            </button>
+            <button onClick={() => removeTodo(id)} className="btn">
+              <i className="far fa-trash-alt"></i>
+            </button>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Notes;
